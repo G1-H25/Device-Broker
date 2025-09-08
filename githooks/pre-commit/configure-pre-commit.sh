9c3c7694d4c1f15706a275e27ec06fe64065027a
LindeborgLove <lindeborglove@gmail.com>
2025-09-08 11:04:03 +0200

    Fixed bug with not installing packages

--- githooks/pre-commit/configure-pre-commit.sh
+++ githooks/pre-commit/configure-pre-commit.sh
@@ -4,10 +4,13 @@ TOPLEVEL=$(git rev-parse --show-toplevel)
 
 if [ ! -e "$TOPLEVEL/.venv" ] ; then
     python3 -m venv $TOPLEVEL/.venv
-	
+
 	source $TOPLEVEL/.venv/bin/activate
-    
-	pip install
+
+	cd $TOPLEVEL/githooks/pre-commit
+	pip install -r requirements.txt
+
+	cd $TOPLEVEL
 	pre-commit install
 fi
 
