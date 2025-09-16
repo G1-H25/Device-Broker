9cee80ccf10e8063e96c2643f72a2515c9a537c8
Love Lindeborg <lindeborglove@gmail.com>
2025-09-16 14:01:12 +0200

    Added data integrity test

--- test/unit/test_memory_buffer/main.cpp
+++ test/unit/test_memory_buffer/main.cpp
@@ -22,26 +22,33 @@ void tearDown() {}
 void test_sensor_buffer_push() {
     storage::MemoryBuffer buffer = storage::MemoryBuffer({ 1 });
 
-    TEST_ASSERT_EQUAL(0, buffer.available());
-    TEST_ASSERT_FALSE(buffer.hasData());
-    TEST_ASSERT_FALSE(buffer.tryPop());
-
-    buffer.pushMeasurement({current_time, temperature, humidity});
+    buffer.pushMeasurement({ 0, 0, 0 });
 
     TEST_ASSERT_EQUAL(1, buffer.available());
     TEST_ASSERT_TRUE(buffer.hasData());
 
     TEST_ASSERT_NOT_NULL(buffer.getLatestMeasurement());
 
-    TEST_ASSERT_EQUAL(current_time, buffer.getLatestMeasurement()->timestamp);
-    TEST_ASSERT_EQUAL(temperature, buffer.getLatestMeasurement()->temperature);
-    TEST_ASSERT_EQUAL(humidity, buffer.getLatestMeasurement()->humidity);
+    buffer.pushMeasurement({ 0, 0, 0 });
+
+    TEST_ASSERT_EQUAL(2, buffer.available());
+    TEST_ASSERT_NOT_NULL(buffer.getLatestMeasurement());
+
+    for (int i = 0; i < buffer.getBufferSize() + 10; i++) {
+        buffer.pushMeasurement({ 0, 0, 0 });
+    }
+
+    TEST_ASSERT_EQUAL(buffer.getBufferSize(), buffer.available());
 }
 
 void test_sensor_buffer_pop() {
     storage::MemoryBuffer buffer = storage::MemoryBuffer({ 1 });
 
-    buffer.pushMeasurement({current_time, temperature, humidity});
+    TEST_ASSERT_EQUAL(0, buffer.available());
+    TEST_ASSERT_FALSE(buffer.hasData());
+    TEST_ASSERT_FALSE(buffer.tryPop());
+
+    buffer.pushMeasurement({ 0, 0, 0 });
 
     TEST_ASSERT_TRUE(buffer.tryPop());
     TEST_ASSERT_FALSE(buffer.hasData());
@@ -51,6 +58,14 @@ void test_sensor_buffer_pop() {
 }
 
 void test_sensor_buffer_data_integrity() {
+    storage::MemoryBuffer buffer = storage::MemoryBuffer({ 1 });
+    buffer.pushMeasurement({current_time, temperature, humidity});
+
+    TEST_ASSERT_EQUAL(*(__uint128_t *) storage::uuid_t{ 1 }.data(), *(__uint128_t *) buffer.getUUID().data());
+
+    TEST_ASSERT_EQUAL(current_time, buffer.getLatestMeasurement()->timestamp);
+    TEST_ASSERT_EQUAL(temperature, buffer.getLatestMeasurement()->temperature);
+    TEST_ASSERT_EQUAL(humidity, buffer.getLatestMeasurement()->humidity);
 }
 
 int main(int argc, char const *argv[]) {
@@ -58,6 +73,7 @@ int main(int argc, char const *argv[]) {
 
     RUN_TEST(test_sensor_buffer_push);
     RUN_TEST(test_sensor_buffer_pop);
+    RUN_TEST(test_sensor_buffer_data_integrity);
 
     return UNITY_END();
 }
