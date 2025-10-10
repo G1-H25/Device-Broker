4776e6361fbd1dc67fdefccb1ef24e08fdb5f736
Love Lindeborg <lindeborglove@gmail.com>
2025-10-10 09:48:42 +0200

    Updated test to print output data

--- test/board/integration/test_http_client/main.cpp
+++ test/board/integration/test_http_client/main.cpp
@@ -11,6 +11,7 @@
 
 #include <unity.h>
 #include <esp_timer.h>
+#include <esp_log.h>
 
 #include "http/http_client.h"
 #include "http/http_response.h"
@@ -23,13 +24,17 @@ using http::HttpClient;
 using http::HttpResponse;
 
 void test_http_client_get() {
-    HttpResponse response = HttpClient::getDriver()->performGetRequest(HTTP_API_HOST, 80, "");
+    HttpResponse response = HttpClient::getDriver()->performGetRequest(HTTP_API_HOST, 443, "");
     TEST_ASSERT_NOT_EQUAL(200, response.status);  // using current value to test if get works
+
+    ESP_LOGI("HTTP_GET_DATA", "%s", response.data.begin());
 }
 
 void test_http_client_post() {
-    HttpResponse response = HttpClient::getDriver()->performPostRequest(HTTP_API_HOST, 80, "", {});
+    HttpResponse response = HttpClient::getDriver()->performPostRequest(HTTP_API_HOST, 443, "", {});
     TEST_ASSERT_EQUAL(400, response.status);  // using current value to test if get works
+
+    ESP_LOGI("HTTP_POST_DATA", "%s", response.data.begin());
 }
 
 extern "C" void app_main() {
@@ -44,6 +49,7 @@ extern "C" void app_main() {
     HttpClient::setDriver(&driver);
 
     RUN_TEST(test_http_client_get);
+    vTaskDelay(pdMS_TO_TICKS(10000));
     RUN_TEST(test_http_client_post);
 
     UNITY_END();
